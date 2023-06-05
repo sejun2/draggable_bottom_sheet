f2b1742f0bf30120b33cc366022c8d173f95bc4c
sejun2 <qpfjf56@gmail.com>
2023-06-05 23:53:13 +0900

    feat blur background when drag up the sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,8 +21,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage(
+      {super.key, required this.title, this.minHeight, this.maxHeight});
 
+  final double? minHeight;
+  final double? maxHeight;
   final String title;
 
   @override
@@ -28,15 +33,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Offset _startOffset = Offset.zero;
   Offset _endOffset = Offset.zero;
   Offset _updateOffset = Offset.zero;
 
   double _bottomContainerHeight = 300;
+
   double get bottomContainerHeight => _bottomContainerHeight;
-  set bottomContainerHeight (double value) {
+
+  set bottomContainerHeight(double value) {
     _bottomContainerHeight = value;
+  }
+
+  double get heightPercentage {
+
+    return (bottomContainerHeight - minHeight) / (maxHeight -minHeight);
+  }
+
+  late double minHeight = widget.minHeight ?? 300;
+  late double maxHeight =
+      widget.maxHeight ?? MediaQuery.of(context).size.height - 100;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
   }
 
   @override
@@ -45,107 +65,125 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.amber,
       body: Stack(
         children: [
-          Expanded(
+          Positioned(
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
               child: ListView(
-            children: [
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 300,),
-            ],
-          )),
+                  const SizedBox(
+                    height: 300,
+                  ),
+                ],
+              )),
+          Positioned(
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                tileMode: TileMode.repeated,
+                  sigmaX: heightPercentage * 20, sigmaY: heightPercentage * 20),
+              child: Container(),
+            ),
+          ),
           Positioned(
             bottom: 0,
             left: 0,
             right: 0,
             child: GestureDetector(
-              onVerticalDragUpdate: (value){
+              onVerticalDragUpdate: (value) {
                 _updateOffset = value.delta;
 
                 final result = bottomContainerHeight - _updateOffset.dy;
 
-                if(result < 300){
+                if (result < 300) {
                   return;
                 }
 
-                if(result > MediaQuery.of(context).size.height - 100){
+                if (result > maxHeight) {
                   return;
                 }
 
@@ -153,33 +191,34 @@ class _MyHomePageState extends State<MyHomePage> {
                   bottomContainerHeight = result;
                 });
               },
-              onVerticalDragEnd: (value){
-                final benchMark = (((MediaQuery.of(context).size.height - 100 ) - 300 )/ 2) + 300;
+              onVerticalDragEnd: (value) {
+                final benchMark = ((maxHeight - 300) / 2) + 300;
 
                 print(benchMark);
 
-                if(bottomContainerHeight < benchMark){
+                if (bottomContainerHeight < benchMark) {
                   setState(() {
                     bottomContainerHeight = 300;
                   });
                   return;
-                }else{
+                } else {
                   setState(() {
-                    bottomContainerHeight = MediaQuery.of(context).size.height - 100;
+                    bottomContainerHeight = maxHeight;
                   });
                   return;
                 }
               },
               child: Container(
                 alignment: Alignment.center,
-                child: Text("Bottom sheet dialog"),
-                height:bottomContainerHeight,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                      color: Colors.redAccent),),
+                height: bottomContainerHeight,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                    color: Colors.redAccent),
+                child: const Text("Bottom sheet dialog"),
+              ),
             ),
           ),
         ],
